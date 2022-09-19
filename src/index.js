5aee769af1e3ac278f732cec5a0d0960db04b197
GerardoASP <gerardo.sanchezp@autonoma.edu.co>
2022-09-19 17:17:33 -0500

    Point 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+
 const port = process.env.PORT || 3000
-app.listen(port,()=>console.log('server listening on port',port))
+const execute_app = express()
+require('dotenv').config()
+
+execute_app.listen(port,()=>console.log('server listening on port',port))
+
+execute_app.get('/',(req,res)=>res.send('Ingenieria de Software 2'))
+
+mongoose
+    .connect(process.env.CONNECTION_STRING_MONGODB)
+    .then(()=>console.log('Connect with mongodb'))
+    .catch((error)=>console.error(error))
